d0e6f5f5e7fa19354c6fbea2c8129bc41f0629dc
Richard Brooks <rickbrooks22@gmail.com>
2019-07-17 23:41:10 -0400

    added comparison operator statements

--- comparisonOperators.py
+++ comparisonOperators.py
@@ -1,2 +1,20 @@
 #Using comparison operators to return boolean values
 
+name='Rick'
+age=30
+
+print("Does %s equal Rick"%name)
+print(name == 'Rick')
+
+print("Does %s not equal Becky"%name)
+print(name != 'Becky')
+
+print("Does %s not equal Rick"%name)
+print(name != 'Rick')
+
+print('Is %s greater than 25'%age)
+print(age > 25)
+
+print("Is 25 less than %s"%age)
+print(25<age)
+
