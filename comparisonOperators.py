d8849d7ead348d983a5c466efed32e898f585de1
Richard Brooks <rickbrooks22@gmail.com>
2019-07-18 11:19:09 -0400

    added logical operators to chain comparison operators

--- comparisonOperators.py
+++ comparisonOperators.py
@@ -18,3 +18,17 @@ print(age > 25)
 print("Is 25 less than %s"%age)
 print(25<age)
 
+
+#Using Logical Operators to chain comparison operators 
+
+print('Does %s equal Rick and does %s equal 30' %(name, age))
+print(name == 'Rick' and age == 30)
+
+print('Does %s equal Rick or does %s equal 25' %(name, age))
+print(name == 'Rick' or age == 25)
+
+print('Does %s equal rick or does %s equal 25' %(name, age))
+print(name == 'rick' or age == 25)
+
+print('Does not %s equal Rick and does %s equal 30' %(name, age))
+print(not name == 'Rick' and age == 30)
